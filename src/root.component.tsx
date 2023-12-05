dc83fb8636298b1174c472ce6a33bf2790275f48
Sahil Prajapati <sahilprj@amazon.com>
2023-12-05 12:46:28 +0530

    Update UI

--- src/root.component.tsx
+++ src/root.component.tsx
@@ -3,7 +3,7 @@ import "./root.component.css";
 export default function Root(props) {
   return (
     <div className="container2">
-      <p>Page 2 App</p>
+      <p>Revamped Page 2 App</p>
     </div>
   );
 }
